dd2e808fec5fc6c4b85c2c5c8d099464d976edd2
NourHassanM <nm5644714@gmail.com>
2023-08-17 13:33:13 -0700

    ooofff

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,16 +4,16 @@
  * @s: input
  * @b: input
  * @n: input
- * Return: s 
+ * Return: s
 */
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
-	
+
 	for (i = 0; n > 0; i++)
-	{	
+	{
 		s[i] = b;
 		n--;
 	}
-	return (s);	
-}	
+	return (s);
+}
